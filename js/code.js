9bd5318a1e241855187586153a71c00e8df151a9
Prashant Sani <contact@prashantsani.com>
2018-11-23 07:46:35 +0530

    attaching js logic to form

--- js/code.js
+++ js/code.js
@@ -1,28 +1,42 @@
-function determineTriangleType(a, b, c){
-  var triangleType;
+(function(){
 
-  if(typeof a !== "number" || typeof b !== "number" || typeof c !== "number"){
-    return "Please Enter the length of sides in digits";
+  function determineTriangleType(a, b, c){
+    var triangleType;
+
+    if(typeof a !== "number" || typeof b !== "number" || typeof c !== "number"){
+      return "Please Enter the length of sides in digits";
+    }
+
+    if ( (a === 0) || (b === 0) || c === 0){
+      return "Side of a triangle cannot be zero";
+    }
+
+    if ( (a === b) && (b === c) ) {
+      triangleType = "equilateral";
+    }
+    else if ( (a === c) || (b === c) || (a === b)){
+      triangleType = "isoseles";
+    }
+    else {
+      triangleType = "scalene";
+    }
+
+    return "The traingle with sides as " + a + ", " + b + ", " + c + " is an " + triangleType + " triangle.";
+
   }
 
-  if ( (a === 0) || (b === 0) || c === 0){
-    return "Side of a triangle cannot be zero";
-  }
+  document.getElementById('triangleType').addEventListener('submit', function(e){
+    e.preventDefault();
 
-  if ( (a === b) && (b === c) ) {
-    triangleType = "equilateral";
-  }
-  else if ( (a === c) || (b === c) || (a === b)){
-    triangleType = "isoseles";
-  }
-  else {
-    triangleType = "scalene";
-  }
+    var message = determineTriangleType(
+      parseInt(document.getElementById('sideA').value),
+      parseInt(document.getElementById('sideB').value),
+      parseInt(document.getElementById('sideC').value)
+    );
 
-  return "The traingle with sides as " + a + ", " + b + ", " + c + "is an " + triangleType + " triangle.";
+    document.getElementById("triangleResult").innerHTML = message;
 
-}
-
-//test
-console.log(determineTriangleType(10,10,10));
-console.log(determineTriangleType(10,10,10));
+    // Always Return false so that page does not refresh
+    return false;
+  });
+})();
